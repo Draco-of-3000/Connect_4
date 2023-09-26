33c223c0368b0208a5319ba2db1f4bd660baf22d
Draco <kevin.tenisson@gmail.com>
2023-09-26 03:27:04 -0700

    Added display board and seperator methods

--- lib/connect_4.rb
+++ lib/connect_4.rb
@@ -37,4 +37,28 @@ class Connect4
         display_board
         make_move(player_one, player_two)
     end
+
+    def display_board
+        puts seperator
+        puts "|  #{@board[0]}  |  #{@board[1]}  |  #{@board[2]}  |  #{@board[3]}  |  #{@board[4]}  |  #{@board[5]}  |  #{@board[6]} |"
+        puts seperator
+        puts "|  #{@board[7]}  |  #{@board[8]}  | #{@board[9]}  | #{@board[10]}  | #{@board[11]}  | #{@board[12]}  | #{@board[13]} |"
+        puts seperator
+        puts "| #{@board[14]}  | #{@board[15]}  | #{@board[16]}  | #{@board[17]}  | #{@board[18]}  | #{@board[19]}  | #{@board[20]} |"
+        puts seperator
+        puts "| #{@board[21]}  | #{@board[22]}  | #{@board[23]}  | #{@board[24]}  | #{@board[25]}  | #{@board[26]}  | #{@board[27]} |"
+        puts seperator
+        puts "| #{@board[28]}  | #{@board[29]}  | #{@board[30]}  | #{@board[31]}  | #{@board[32]}  | #{@board[33]}  | #{@board[34]} |"
+        puts seperator
+        puts "| #{@board[35]}  | #{@board[36]}  | #{@board[37]}  | #{@board[38]}  | #{@board[39]}  | #{@board[40]}  | #{@board[41]} |"
+        puts seperator
+        puts "\n"
+    end
+
+    def seperator
+        "-----+-----+-----+-----+-----+-----+------"
+    end
 end
+
+game = Connect4.new
+game.display_board
